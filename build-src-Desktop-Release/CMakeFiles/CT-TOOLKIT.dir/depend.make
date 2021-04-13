9d9b9069da2ab2b87f3affb0ed28062e2f271f8f
Michele Perrone <perrone.michele@outlook.com>
2021-04-13 15:47:39 +0200

    Update gitignore

--- build-src-Desktop-Release/CMakeFiles/CT-TOOLKIT.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for CT-TOOLKIT.
-# This may be replaced when dependencies are built.
--- build-src-Desktop-Release/CMakeFiles/CT-TOOLKIT_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/CT-TOOLKIT_autogen"
-  "CT-TOOLKIT_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/CT-TOOLKIT_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
